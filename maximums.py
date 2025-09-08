8635b61e3b2df3b3d94c606a0ae59f700827db1c
Mariajosehid <mjhidalgo@mail.austral.edu.ar>
2025-09-08 15:49:49 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -11,34 +11,3 @@ def max_of_three(x, y, z):
         return y
     else:
         return z
-
-# max_of_two (3 veces)
-print("Probando max_of_two")
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-print("El mayor es:", max_of_two(x, y))
-
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-print("El mayor es:", max_of_two(x, y))
-
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-print("El mayor es:", max_of_two(x, y))
-
-# max_of_three (3 veces)
-print("\nProbando max_of_three")
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-z = int(input("Ingresa z: "))
-print("El mayor es:", max_of_three(x, y, z))
-
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-z = int(input("Ingresa z: "))
-print("El mayor es:", max_of_three(x, y, z))
-
-x = int(input("Ingresa x: "))
-y = int(input("Ingresa y: "))
-z = int(input("Ingresa z: "))
-print("El mayor es:", max_of_three(x, y, z))
